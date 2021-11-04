cc4af525cfde006b31de74f99fa0c1eda30748a3
sykycao <sykycao@gmail.com>
2021-11-04 10:46:05 +0700

    create HOC withInstrument  and use it with Keyboard component

--- src/components/Keyboard/WithInstrument.tsx
+++ src/components/Keyboard/WithInstrument.tsx
@@ -1,17 +1,15 @@
 import { Keyboard } from './Keyboard';
 import { useAudioContext } from '../AudioContextProvider';
 import { useInstrument } from '../../state/Instrument';
-import { SoundfontProvider } from '../../adapters/Soundfont';
+import { withInstrument } from '../../adapters/Soundfont/withInstrument';
+
+const WrappedKeyboard = withInstrument(Keyboard);
 
 export const KeyboardWithInstrument = () => {
   const AudioContext = useAudioContext()!;
   const { instrument } = useInstrument();
 
   return (
-    <SoundfontProvider
-      AudioContext={AudioContext}
-      instrument={instrument}
-      render={(props) => <Keyboard {...props} />}
-    />
+    <WrappedKeyboard AudioContext={AudioContext} instrument={instrument} />
   );
 };
